66409a437ff17f24c9f9662de3b82ec90e51f8b3
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-10-24 18:53:13 +0200

    Design model

---
+++ src/main/java/ua/mibal/model/Record.java
@@ -0,0 +1,24 @@
+package ua.mibal.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Record {
+    private Long id;
+    private Long userId;
+    private Long categoryId;
+    private LocalDateTime timestamp;
+    private Long amount;
+}
---
+++ src/main/java/ua/mibal/model/User.java
@@ -0,0 +1,19 @@
+package ua.mibal.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    private Long id;
+    private String name;
+}
