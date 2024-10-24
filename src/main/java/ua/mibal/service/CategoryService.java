069d95c69a6faa2cb940e8e858c21513e966f990
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-10-24 20:02:46 +0200

    Implement CategoryService

--- src/main/java/ua/mibal/service/CategoryService.java
+++ src/main/java/ua/mibal/service/CategoryService.java
@@ -1,16 +1,20 @@
 package ua.mibal.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ua.mibal.model.Category;
+import ua.mibal.repository.Repository;
 
 /**
  * @author Mykhailo Balakhon
  * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
  */
+@RequiredArgsConstructor
 @Service
 public class CategoryService {
+    private final Repository<Category, Long> repository;
 
     public Category create(Category category) {
-        return null;
+        return repository.save(category);
     }
 }
