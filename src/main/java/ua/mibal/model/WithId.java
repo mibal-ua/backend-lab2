fd25eafa6ac610c674231d5fe6bb9e4e57fe9bc6
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-10-24 20:07:55 +0200

    Implement InMemoryRepository

---
+++ src/main/java/ua/mibal/model/WithId.java
@@ -0,0 +1,12 @@
+package ua.mibal.model;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
+ */
+public interface WithId {
+
+    Long getId();
+
+    void setId(Long id);
+}
