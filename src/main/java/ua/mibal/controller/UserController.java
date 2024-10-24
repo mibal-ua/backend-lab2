44a2466c3b6a3c915a0b6956789f7e2712c45bb6
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-10-24 18:58:36 +0200

    Design UserController

---
+++ src/main/java/ua/mibal/controller/UserController.java
@@ -0,0 +1,39 @@
+package ua.mibal.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ua.mibal.model.User;
+
+import java.util.List;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
+ */
+@RestController
+public class UserController {
+
+    @GetMapping("/user/{userId}")
+    public User get(@PathVariable Long userId) {
+        return null;
+    }
+
+    @DeleteMapping("/user/{userId}")
+    public User delete(@PathVariable Long userId) {
+        return null;
+    }
+
+    @PostMapping("/user")
+    public User get(@RequestBody User user) {
+        return null;
+    }
+
+    @GetMapping("/users")
+    public List<User> getAll() {
+        return null;
+    }
+}
