ec073eab4768fbf5ad2c0b5f20dadb1570fec7d2
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-10-24 18:49:29 +0200

    Init version

---
+++ src/main/java/ua/mibal/BackendLab2Application.java
@@ -0,0 +1,13 @@
+package ua.mibal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendLab2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ua/mibal/BackendLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.mibal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
