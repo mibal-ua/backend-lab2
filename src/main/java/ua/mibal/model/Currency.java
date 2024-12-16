a17104f7f72a2d7750478b9e20bd69907e231dff
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-12-16 17:25:00 +0100

    Design `Currency` by requirements

--- src/main/java/ua/mibal/model/Currency.java
+++ src/main/java/ua/mibal/model/Currency.java
@@ -1,5 +1,12 @@
 package ua.mibal.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,10 +16,18 @@ import lombok.Setter;
  * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
  */
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Getter
 @Setter
+@Entity
+@Table(name = "currencies")
 public class Currency {
+
+    @Id @GeneratedValue
     private Long id;
+
+    @Column(nullable = false)
     private String name;
     
     public Currency(String name) {
