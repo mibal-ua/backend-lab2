64a100b77c042d4cd9152684be0899f3777d896b
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-12-16 17:28:25 +0100

    Design `User` by requirements

--- src/main/java/ua/mibal/model/User.java
+++ src/main/java/ua/mibal/model/User.java
@@ -1,5 +1,12 @@
 package ua.mibal.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,8 +22,17 @@ import lombok.Setter;
 @Builder
 @Getter
 @Setter
+@Entity
+@Table(name = "users")
 public class User implements WithId {
+    
+    @Id @GeneratedValue
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+    
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "default_currency_id")
     private Currency defaultCurrency;
 }
